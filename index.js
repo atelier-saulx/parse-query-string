7fe2279da40ed7c1558713d814e4ebdb9987d684
youzi <youri@saulx.com>
2022-01-05 17:16:10 +0100

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const reducer = (params, str) => {
+  const [key, value] = str.split("=");
+  if (key) {
+    params[decodeURIComponent(key)] =
+      value === undefined
+        ? true
+        : isNaN(value)
+        ? decodeURIComponent(value)
+        : Number(value);
+  }
+  return params;
+};
+
+const parseQueryString = (string = location?.search || "") =>
+  string.split(/\?|&/).reduce(reducer, {});
+
+export default parseQueryString;
